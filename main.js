f7bd064d1c611aab535395dfcda0991b0177fcc4
Raphi Spoerri <140029470+RaphiSpoerri@users.noreply.github.com>
2024-07-04 00:24:50 -0400

    Update main.js

--- main.js
+++ main.js
@@ -12,10 +12,11 @@ class Jastrow {
 		const results = await response.json();
 		const found = [];
 		for (const entry of results) {
-			if (entry['parent_lexicon'] == 'Jastrow Dictionary')
+			if (entry['parent_lexicon'] == 'Jastrow Dictionary') {
 				found.push(`${entry.headword}, ${(entry.alt_headwords ?? []).join(", ")} ${
 					entry.content.senses.map(a => `<p>*) + ${a.definition}</p>`).join()}`);
     			console.log(found);
+			}
 		}
 		return found;
 	}
