d4cbee28b9cc3ed1c6ba1ffa95965b77e0e428b4
Raphi Spoerri <140029470+RaphiSpoerri@users.noreply.github.com>
2024-07-03 20:06:50 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,22 @@
+
+const E = {
+  word: document.getElementById("word"),
+  dictResult: document.getElementById("result"),
+};
+class Jastrow {
+	static async serverLookup(word) {
+		const options = { method: 'GET', headers: { accept: 'application/json'} }
+		const response = await fetch(`https://www.sefaria.org/api/words/${word}`, options);
+		const results = await response.json();
+		const found = [];
+		for (const entry of results) {
+			if (entry['parent_lexicon'] == 'Jastrow Dictionary')
+				entry['content']['senses'].forEach(a => found.push(a['definition']));
+		}
+    console.log(found);
+		return found;
+	}
+}
+async function search() {
+  E.dictResult.innerHTML = (await Jastrow.serverLookup(E.word.value)).join("<br>");
+}
